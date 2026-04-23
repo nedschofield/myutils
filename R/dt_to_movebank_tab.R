6bec1001ea76a9cf127d6816710843e1c657eae6
nedschofield <nedscho555@gmail.com>
2026-04-23 14:08:35 +0930

    updated function so that it works.

--- R/dt_to_movebank_tab.R
+++ R/dt_to_movebank_tab.R
@@ -2,10 +2,10 @@
 #'
 #' @description
 #' author: Ned Ryan-Schofield
-#' date: 03/03/2026
+#' date: 23/04/2026
 #' Helper function for coercing a list of data.table objects of gps data that have been checked for errors and coerced back to data.table to a single .txt file for upload to movebank.
 #'
-#' @param dt a list of data.tables that are gps data
+#' @param dt a list of data.tables that are gps data, with same column names
 #' @param outpath character variable of file path name. i.e. "./outputs/table/tiwi_cats_movebank_up_03032026.txt"
 #' @param timestamp POSIXct variable - data.table timestamp column
 #' @param x numerical variable - data.table coordinate longitude column
@@ -13,22 +13,45 @@
 #' @param ID character variable - data.table animal ID column
 #' @returns writes a textfile to disc.
 
-DT_to_movebank_tab <- function(dt, out.path, t = "timestamp", x = "long", y = "lat", ID = "ID") {
+DT_to_movebank_tab <- function(dt, out.path,
+                               t = "timestamp",
+                               y = "lat",
+                               x = "long",
+                               ID = "ID") {
 
   ### checks ###
   if (!inherits(dt, "list")) {
-    stop("`", dt, "` is not a list of data.table objects")
+    stop("`dt` must be a list of data.table objects")
   }
 
-  dt.long <- do.call(rbind, dt)
-  names(dt.long) <- c("timestamp", "location.lat", "location.long", "individual.local.identifier") #rename to movebank naming conventions
+  #bind lists together
+  dt.long <- data.table::rbindlist(dt, use.names = TRUE, fill = TRUE)
 
-  write.table(
+  #check no missing columns
+  req <- c(t, y, x, ID)
+  miss <- setdiff(req, names(dt.long))
+  if (length(miss)) {
+    stop("Missing required columns: ", paste(miss, collapse = ", "))
+  }
+
+  out <- dt.long[, .SD, .SDcols = req]
+
+  #rename to movebank naming conventions
+  data.table::setnames(
+    out,
+    old = c(t, y, x, ID),
+    new = c("timestamp", "location.lat", "location.long", "individual.local.identifier")
+  )
+
+  #write to disc
+  utils::write.table(
     x = dt.long,
     file = out.path,
     sep = "\t",
     row.names = FALSE,
     quote = FALSE
   )
+
+  invisible(out)
 
 }
